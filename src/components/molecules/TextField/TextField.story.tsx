dafad51f30bd6c4e671de0734ed646e4f8d1d9eb
Wafaa Nasr <wafaa.aanasr@gmail.com>
2020-10-11 22:16:10 +0200

    add molecules textfield documentation with linting

--- src/components/molecules/TextField/TextField.story.tsx
+++ src/components/molecules/TextField/TextField.story.tsx
@@ -6,11 +6,14 @@ import TextField from './TextField.component';
 import data from './TextField.data';
 import markdown from './TextField.story.md';
 
-storiesOf('components/molecules/TextField', module).add('TextField', () => (
-    <StoryContainer>
-        <TextField {...data} />
-    </StoryContainer>
-)
-    , {
-        notes: markdown
-    });
+storiesOf('components/molecules/TextField', module).add(
+    'TextField',
+    () => (
+        <StoryContainer>
+            <TextField {...data} />
+        </StoryContainer>
+    ),
+    {
+        notes: markdown,
+    },
+);
