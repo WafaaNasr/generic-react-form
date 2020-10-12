3bb0f82c19b7c52f891582f407e9d348926db394
Wafaa Nasr <wafaa.aanasr@gmail.com>
2020-10-12 09:53:24 +0200

    after linting still missing snapshot

--- src/components/atoms/Label/Label.test.tsx
+++ src/components/atoms/Label/Label.test.tsx
@@ -1,12 +1,10 @@
-import React from "react";
-import { render } from "@testing-library/react";
+import React from 'react';
+import { render } from '@testing-library/react';
 
-import Label from "./Label.component";
+import Label from './Label.component';
 
-describe("Label", () => {
-    test("should display a blank login form, with remember me checked by default", async () => {
-       
-        return render(
-            <Label htmlFor='text'>Hiii</Label>);
+describe('Label', () => {
+    test('should display a blank login form, with remember me checked by default', async () => {
+        return render(<Label htmlFor="text">Hiii</Label>);
     });
 });
--- src/components/organisms/Form/Form.component.tsx
+++ src/components/organisms/Form/Form.component.tsx
@@ -45,7 +45,7 @@ const Form = <T extends Record<string, any>>({
     ) : formState.isSubmitSuccessful ? (
         <Alert text={successMessage} messageType={MessageType.Success} />
     ) : (
-        <FormContainer data-testid='form' onSubmit={handleSubmit(onSubmitCb)}>
+        <FormContainer data-testid="form" onSubmit={handleSubmit(onSubmitCb)}>
             {formFields.map((textField) => (
                 <TextField
                     {...textField}
@@ -56,7 +56,7 @@ const Form = <T extends Record<string, any>>({
                 />
             ))}
             <Button
-               data-testid='submit-btn'
+                data-testid="submit-btn"
                 type="submit"
                 parentHasErrors={hasErrors > 0 || formState.isSubmitting || formState.isSubmitSuccessful}
             >
